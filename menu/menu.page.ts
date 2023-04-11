43edb94754e6970ec7e0c1de908a8e3232d53d15
ameeeni <amenibramli1@gmail.com>
2023-04-11 13:08:10 +0200

    second-commit

--- menu/menu.page.ts
+++ menu/menu.page.ts
@@ -10,7 +10,8 @@ export class MenuPage implements OnInit {
   pages = [
     {
       title:'Comptes',
-      children : [
+      icon : 'briefcase-outline',
+      children  : [
         {
           title:'Consulter mes comptes ',
           url :'menu/consulterComptes',
@@ -28,10 +29,70 @@ export class MenuPage implements OnInit {
           url :'menu/importerCompte',
           icon:'briefcase-outline'
         }
+      ],
+    }, {
+    title : 'Operations',
+      icon : 'briefcase-outline',
+      children: [
+        {
+          title:'Virements ',
+          url :'menu/consulterComptes',
+          icon:'briefcase-outline'
+        }, {
+          title:'Cartes ',
+          url :'menu/dashbordAchat',
+          icon:'briefcase-outline'
+        }
+      ]
+    }, {
+      title : 'Profiles',
+      icon : 'briefcase-outline',
+      children: [
+        {
+          title:'Informations Personnelles ',
+          url :'menu/consulterComptes',
+          icon:'briefcase-outline'
+        }, {
+          title:'Changer le mot de passe ',
+          url :'menu/dashbordAchat',
+          icon:'briefcase-outline'
+        }
+      ]
+
+    }, {
+    title:'Messangerie',
+      icon : 'briefcase-outline',
+      children: [
+        {
+          title:'Envoyer un message au Support ',
+        }, {
+          title:'Consulter mes Messages'
+        }
       ]
     }
     ]
-  constructor() { }
+   infos = [
+     {
+       title : "Notifications",
+     },
+     {
+       title : "Réseau Postal et file d'attente",
+     },
+     {
+       title : "Cours de Change ",
+     },
+     {
+       title : "A propos myPosteBank",
+     },
+     {
+       title : "Comment ça marche",
+     },{
+       title : "FAQ",
+     }
+   ]
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
